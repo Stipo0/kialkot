383a8dbc82839acd7acecc7a2caabe8f116ac10e
Galgóczi Áron <57896206+Kovis97@users.noreply.github.com>
2022-10-10 11:59:07 +0200

    navbar-feture positioning Logo

--- kialkot/kialkot/ClientApp/src/components/NavMenu.js
+++ kialkot/kialkot/ClientApp/src/components/NavMenu.js
@@ -26,7 +26,7 @@ export class NavMenu extends Component {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/" className='w-25'><img src={Logo} alt="logo" className='w-50 rounded float-left'/></NavbarBrand>
+          <NavbarBrand tag={Link} to="/"><img src={Logo} alt="logo" width="120"/></NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
