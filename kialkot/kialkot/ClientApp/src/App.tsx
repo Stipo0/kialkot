a9687bd36f89f36f337a4e149d63cbe3313a7531
Kovis97 <galgoczi.aron97@gmial.com>
2022-10-30 10:52:57 +0100

    Refakt

--- kialkot/kialkot/ClientApp/src/App.tsx
+++ kialkot/kialkot/ClientApp/src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from "./components/navbar/Navbar";
+
+interface AppProps {}
+
+interface AppState {
+  counter: number;
+}
+
+class App extends Component<AppProps, AppState> {
+  readonly state: AppState = { counter: 0 };
+
+  setCounterValue = (increase: boolean) => {
+    this.setState(({ counter }) => {
+      const newValue = increase ? counter + 1 : counter - 1;
+      return { counter: newValue };
+    });
+  };
+
+  clearValue = () => {
+    this.setState({ counter: 0 });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+        </header>
+        <div className="container d-flex justify-content-center">
+          <div className="card my-4  p-4 bg-white shadow text-center">
+            <h5>Counter: {this.state.counter}</h5>
+            <div className="d-flex justify-content-center flex-wrap gap-2">
+              <button
+                className="btn btn-primary"
+                onClick={() => this.setCounterValue(true)}
+              >
+                Increase +
+              </button>
+              <button
+                className="btn btn-secondary"
+                onClick={() => this.setCounterValue(false)}
+              >
+                Decrease -
+              </button>
+              <button className="btn btn-danger" onClick={this.clearValue}>
+                Clear
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
