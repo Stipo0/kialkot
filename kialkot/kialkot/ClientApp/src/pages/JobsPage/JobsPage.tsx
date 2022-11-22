c9724934a42fa8cfdd145fab79119859949c1726
Kovis97 <galgoczi.aron97@gmail.com>
2022-11-22 16:10:15 +0100

    some refakt

--- kialkot/kialkot/ClientApp/src/pages/JobsPage/JobsPage.tsx
+++ kialkot/kialkot/ClientApp/src/pages/JobsPage/JobsPage.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import JobCard from "../../components/job-card/JobCard";
 
 import Page from "../../components/page/Page";
@@ -8,7 +7,6 @@ import { jobsService } from "../../service/job.service";
 
 const JobsPage = () => {
   const [jobs, setJobs] = useState<MinJobModel[]>([]);
-  const navigation = useNavigate();
 
   useEffect(() => {
     const fetchJobs = async () => {
@@ -22,7 +20,10 @@ const JobsPage = () => {
     <Page title="Munkák" noCard>
       <div className="row">
         {jobs.map((job) => (
-          <div key={job.id} onClick={()=> navigation("/jobs/" + job.id)} className="col-lg-4 col-md-6 col-sm-12">
+          <div
+            key={job.id}
+            className="col-lg-4 col-md-6 col-sm-12"
+          >
             <JobCard job={job} />
           </div>
         ))}
