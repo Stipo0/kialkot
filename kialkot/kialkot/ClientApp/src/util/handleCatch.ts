36e238fe1b93db350c0c9202ee9c837653b0281d
Kovis97 <galgoczi.aron97@gmail.com>
2022-11-26 18:27:51 +0100

    Refakt problem handling

--- kialkot/kialkot/ClientApp/src/util/handleCatch.ts
+++ kialkot/kialkot/ClientApp/src/util/handleCatch.ts
@@ -1,10 +1,15 @@
 import { AxiosError } from "axios";
+import { ResponseModel } from "../models/response.model";
 
 export function HanleCatch(error: unknown) {
   if (error instanceof AxiosError) {
     if (error.response) {
       console.log(error.response);
-      return `${error.response.status} - ${error.response.data.error}`;
+      const data: ResponseModel = error.response.data;
+      console.log(data.title);
+      
+      let message = data.error ? data.error : (data.title ? data.title : data)
+      return `${error.response.status} - ${message}`;
     } else if (error.request) {
       console.log(error.request);
       return "Network problem!";
