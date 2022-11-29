985f17bbc23f8ba8d12269a7ab4f265bc300325d
Kovis97 <galgoczi.aron97@gmail.com>
2022-11-29 15:16:09 +0100

    Add jobStatus filter

--- kialkot/kialkot/ClientApp/src/enums/job.status.enum.ts
+++ kialkot/kialkot/ClientApp/src/enums/job.status.enum.ts
@@ -1,3 +1,5 @@
+import { OptionValues } from "../components/text-field/TextField";
+
 export enum JobStatusEnum {
   Open,
   Canceled,
@@ -7,7 +9,14 @@ export enum JobStatusEnum {
 }
 
 export namespace JobStatusEnum {
-	export function toString(jobStatus: JobStatusEnum): string {
-		return JobStatusEnum[jobStatus];
-	}
- }
+  export function toString(jobStatus: JobStatusEnum): string {
+    return JobStatusEnum[jobStatus];
+  }
+
+  export const toOptions: OptionValues[] = Object.values(JobStatusEnum)
+    .filter((value) => typeof value === "string")
+    .map((value) => ({
+      value: JobStatusEnum[value as number],
+      name: value as string,
+    }));
+}
