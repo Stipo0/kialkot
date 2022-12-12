45b09f0859550ff9487c20a6f507a797f681837c
Kovis97 <galgoczi.aron97@gmail.com>
2022-12-12 20:13:51 +0100

    add User filter

--- kialkot/kialkot/ClientApp/src/pages/AdminPage/AdminPage.tsx
+++ kialkot/kialkot/ClientApp/src/pages/AdminPage/AdminPage.tsx
@@ -1,34 +1,82 @@
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 import Page from "../../components/page/Page";
+
 import { AdminMinUSerModel } from "../../models/user.model";
+
 import { userService } from "../../service/user.service";
+
 import { HanleCatch } from "../../util/handleCatch";
+
+export interface RoleTypeProps {
+  role: "User" | "Designer" | "UserAndDesigner";
+}
 
 const AdminPage = () => {
   const [users, setUsers] = useState<AdminMinUSerModel[]>([]);
+  const navigate = useNavigate();
+
+  const fetchUsers = async (role: RoleTypeProps) => {
+    try {
+      setUsers(await userService.getUsers(role));
+    } catch (e) {
+      alert(HanleCatch(e));
+    }
+  };
 
   useEffect(() => {
     const fetchUsers = async () => {
-			try {
-				setUsers(await userService.getUsers());
-			} catch (e) {
-				alert(HanleCatch(e));
-			}
+      try {
+        setUsers(await userService.getUsers({ role: "User" }));
+      } catch (e) {
+        alert(HanleCatch(e));
+      }
     };
-
     fetchUsers();
   }, []);
 
   return (
     <Page title="A regisztrált felhasználók:">
+      <div className="row ms-auto me-auto mb-3">
+        <label htmlFor="Role" className="col-auto">
+          <input
+            type="radio"
+            name="Role"
+            className="me-1"
+            onClick={() => fetchUsers({ role: "User" })}
+          />
+          Munkadó
+        </label>
+        <label htmlFor="Role" className="col-auto">
+          <input
+            type="radio"
+            name="Role"
+            className="me-1"
+            onClick={() => fetchUsers({ role: "Designer" })}
+          />
+          Grafikus
+        </label>
+        <label htmlFor="Role" className="col-auto">
+          <input
+            type="radio"
+            name="Role"
+            className="me-1"
+            onClick={() => fetchUsers({ role: "UserAndDesigner" })}
+          />
+          Munkadó és Grafikus
+        </label>
+      </div>
       <table>
         <thead>
           <tr>
             <th>Azonosító</th>
             <th>Felhasználó név</th>
             <th>Email</th>
-            <th></th>
+            <th>Módosítás</th>
+            <th>Törlés</th>
           </tr>
         </thead>
         <tbody>
@@ -38,7 +86,13 @@ const AdminPage = () => {
               <td>{user.nickName}</td>
               <td>{user.email}</td>
               <td>
-                <Link to={`/admin/user/${user.id}`}>Módosít</Link>
+                <FontAwesomeIcon
+                  icon={faEdit}
+                  onClick={() => navigate(`/admin/user/${user.id}`)}
+                />
+              </td>
+              <td>
+                <FontAwesomeIcon icon={faTrash} />
               </td>
             </tr>
           ))}
--- kialkot/kialkot/ClientApp/src/service/user.service.ts
+++ kialkot/kialkot/ClientApp/src/service/user.service.ts
@@ -4,14 +4,22 @@ import {
   UpdateCredentialsModel,
   UserModel,
 } from "../models/user.model";
+import { RoleTypeProps } from "../pages/AdminPage/AdminPage";
 import request, { Methods } from "../util/request";
 
 class UsersService {
-  async getUsers() {
-		return request<AdminMinUSerModel[]>({
-			method: Methods.GET,
-			resource: 'api/users',
-		})
+  async getUsers(role: RoleTypeProps) {
+    return request<AdminMinUSerModel[]>({
+      method: Methods.GET,
+      resource: `api/Account/users/${role.role}`,
+    });
+  }
+
+  async getUser(id: string) {
+    return request<UserModel>({
+      method: Methods.GET,
+      resource: `api/Account/user/${id}`,
+    });
   }
 
   async storeMe(credentials: StoreCredentialsModel) {
