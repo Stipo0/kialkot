a4a4f57631eee67f33af7b07dd44aff0e807d09d
Kovis97 <galgoczi.aron97@gmail.com>
2022-12-12 16:42:30 +0100

    AdminPage refakt

--- kialkot/kialkot/ClientApp/src/pages/AdminPage/AdminPage.tsx
+++ kialkot/kialkot/ClientApp/src/pages/AdminPage/AdminPage.tsx
@@ -1,7 +1,51 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Page from "../../components/page/Page";
+import { AdminMinUSerModel } from "../../models/user.model";
+import { userService } from "../../service/user.service";
+import { HanleCatch } from "../../util/handleCatch";
+
 const AdminPage = () => {
-	return (
-		<p>Szia ez itt az admin felület!</p>
-	)
+  const [users, setUsers] = useState<AdminMinUSerModel[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+			try {
+				setUsers(await userService.getUsers());
+			} catch (e) {
+				alert(HanleCatch(e));
+			}
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <Page title="A regisztrált felhasználók:">
+      <table>
+        <thead>
+          <tr>
+            <th>Azonosító</th>
+            <th>Felhasználó név</th>
+            <th>Email</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.nickName}</td>
+              <td>{user.email}</td>
+              <td>
+                <Link to={`/admin/user/${user.id}`}>Módosít</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Page>
+  );
 };
 
 export default AdminPage;
