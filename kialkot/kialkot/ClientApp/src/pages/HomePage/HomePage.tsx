6effec134792059831feda71c54fb19fcad59c0a
Kovis97 <galgoczi.aron97@gmail.com>
2022-12-12 16:40:50 +0100

    Home page 0.1

---
+++ kialkot/kialkot/ClientApp/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+const HomePage = () => {
+	return (
+		<div className="HomePage">
+			<pre>
+				Üdvözlünk oldalunkon!
+
+				Ezt odalt a JóKérdés csapat alkotta a Projectmunka 2 tárgy teljesítésére.
+			</pre>
+		</div>
+	)
+};
+
+export default HomePage;
